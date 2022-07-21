e06ce5b5387268afa69f43362b79e9bfdf61435e
Zahra-Bens <107687115+Zahra-Bens@users.noreply.github.com>
2022-07-20 21:21:45 -0400

    added a form

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let city = "Paris";
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -12,6 +14,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `Humidity: ${response.data.main.humidity} %`;
   windElement.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function formatDate(timestamp) {
@@ -19,8 +26,14 @@ function formatDate(timestamp) {
 
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0 ${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -30,9 +43,21 @@ function formatDate(timestamp) {
     "Friday",
     "Saturday",
   ];
+  let day = days[date.getDay()];
   return `${day} ${hours}: ${minutes}`;
 }
 
-let apiKey = "689549cb85d358f26d385996b4d2dcfb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "689549cb85d358f26d385996b4d2dcfb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
